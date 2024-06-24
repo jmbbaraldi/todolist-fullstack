0721bd0254b7d0f37fe9ea12f9ff027fec0ab353
João Baraldi <jmbbaraldi@gmail.com>
2024-06-24 06:55:45 -0300

    front-ent: First steps

---
+++ toDoList-frontend/js/script.js
@@ -0,0 +1,21 @@
+const fetchTask = async () => {
+    const response = await fetch('http://localhost:3333/tasks');
+    const tasks = response.json();
+
+    return tasks;
+}
+
+const createElement = (tag, innerText = '') => {
+    const element = document.createElement(tag);
+    element.innerText = innerText;
+    return element;
+}
+
+const createRow = (task) => {
+
+    const{ id, title, created_at, status } = task;
+
+    const tr = createElement('tr')
+    const tdTitle = createElement('td');
+    
+}
